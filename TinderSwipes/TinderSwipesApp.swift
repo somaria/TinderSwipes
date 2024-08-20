eec313521d7a5c431f43271d0780c424fa4d7a43
somaria <daryl.9@gmail.com>
2024-08-20 22:48:58 +0800

    Initial Commit

---
+++ TinderSwipes/TinderSwipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderSwipesApp.swift
+//  TinderSwipes
+//
+//  Created by daryl on 20/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TinderSwipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
